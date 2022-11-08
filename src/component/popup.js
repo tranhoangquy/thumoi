415feea9f31d91db87ef0eeca1d875e94dbf007d
quyth <tranhoangquy98@gmail.com>
2022-11-09 00:38:22 +0700

    a

--- src/component/popup.js
+++ src/component/popup.js
@@ -2,24 +2,26 @@ import { useState } from 'react';
 import '../App.css';
 
 function Popup() {
-    const [name,setName] = useState()
-    const changeName = (event)=> {
-        console.log(event.target.value)
-        setName(event.target.value)
+  const [name, setName] = useState()
+  const changeName = (event) => {
+    console.log(event.target.value)
+    setName(event.target.value)
+  }
+  const nhapten = () => {
+    localStorage.setItem('nameF10', name)
+    if(!name){
     }
-    const nhapten = ()=>{
-        localStorage.setItem('nameF10',name)
-    }
+  }
 
   return (
-   
-      <div className='propup'>
-        <div style={{marginBottom:'5px'}}>Nhập tên khách mời (tên mình)</div>
-        <form onSubmit={nhapten}>
+
+    <div className='propup'>
+      <div style={{ marginBottom: '5px' }}>Nhập tên khách mời (tên mình)</div>
+      <form onSubmit={nhapten}>
         <input placeholder='Vui lòng nhập tên' type="text" id="lname" name="lname" onChange={changeName} /><br></br>
-        <button type='submit' value="submit" style={{marginTop:'15px'}}> Click để nhập thiệp</button>
-        </form>
-      </div>
+        <button type='submit' value="submit" style={{ marginTop: '15px' }} disabled={!name && true} id="btn"> Click để nhận thiệp</button>
+      </form>
+    </div>
 
   );
 }
